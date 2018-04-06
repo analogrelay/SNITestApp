3ee82e4fcd07c3097ad9634d3033e8ec70691993
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2018-04-06 09:49:47 -0700

    initial app

---
+++ SNITestApp/CertificateManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Concurrent;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SNITestApp
+{
+    public class CertificateManager
+    {
+        private ConcurrentDictionary<string, X509Certificate2> _certificates = new ConcurrentDictionary<string, X509Certificate2>();
+
+        public X509Certificate2 GetCertificate(string serverName)
+        {
+            return _certificates.GetOrAdd(serverName, LoadCertificate);
+        }
+
+        private static X509Certificate2 LoadCertificate(string serverName)
+        {
+            var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadOnly);
+            var certificates = store.Certificates.Find(X509FindType.FindBySubjectName, serverName, validOnly: false);
+            if(certificates.Count > 0)
+            {
+                return certificates[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ SNITestApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace SNITestApp
+{
+    public class Program
+    {
+        private static readonly CertificateManager _certificateManager = new CertificateManager();
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options =>
+                {
+                    options.ConfigureHttpsDefaults(httpsOptions =>
+                    {
+                        httpsOptions.ServerCertificateSelector = SelectCertificate;
+                    });
+                })
+                .UseStartup<Startup>();
+
+        private static X509Certificate2 SelectCertificate(IFeatureCollection connectionFeatures, string serverName)
+        {
+            Console.WriteLine("Requested server: " + serverName);
+            
+            // Look up in the cert store
+            return _certificateManager.GetCertificate(serverName);
+        }
+    }
+}
